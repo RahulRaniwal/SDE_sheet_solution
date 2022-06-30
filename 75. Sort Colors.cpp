d4cf97bcc96fe5b3c1ca258a17abcfd30a3e6fd3
Gojo_69 <59061631+RahulRaniwal@users.noreply.github.com>
2022-06-30 23:18:58 +0530

    sort color and Dutch National flag
    in DNS sort take 3 pointer low = 0 , mid = 0 , high = n-1
    traverse till mid <= high
    case1 (nums[mid] == 0)   swap(low ,mid) low++ mid++
    case2(nums[mid] == 1) mid++;
    case3 (nums[mid] == 2)   swap(high , mid) high--;

--- 75. Sort Colors.cpp
+++ 75. Sort Colors.cpp
@@ -34,4 +34,35 @@ public:
     }
 };
 
-/************************* TC O(n)  ****************************/
+/************************* TC O(n) + O(n)  ****************************/
+
+
+// Approach 2 DNS Dutch National Flag
+
+class Solution {
+public:
+    // Dutch National Flag
+    void sortColors(vector<int>& nums){
+        int n = nums.size();
+        int low = 0;
+        int mid = 0;
+        int high = n-1;
+        
+        while(mid <= high){
+            // if value is zero
+            if(nums[mid] == 0){
+                swap(nums[low] , nums[mid]);
+                low++;
+                mid++;
+            }else if(nums[mid] == 1){   // if value is 1
+                mid++;
+            }else{                      // if value is 2
+                swap(nums[mid] , nums[high]);   
+                high--;
+            }
+        }
+        
+    }
+};
+
+/************** TC O(n)  ***************/
