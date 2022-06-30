3f4d97937edc14d5fe950888681b57a09f8038e0
Gojo_69 <59061631+RahulRaniwal@users.noreply.github.com>
2022-06-30 22:52:25 +0530

    Sort Colors or Dutch National Flag
    Approach
    St1: Count no. of zero, one, and two
    St2: Again travel in the array and place zero then one and at last two.
    That's it!

---
+++ 75. Sort Colors.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    // dns Sort (Dutch National Flag)
+    void sortColors(vector<int>& nums) { 
+        int count0 = 0 , count1 = 0 , count2 = 0; 
+      // traverse the array to keep count of number of 0 , 1 and 2.
+        for(int i = 0; i < nums.size(); i++){
+            switch(nums[i]){
+                case 0:
+                    count0++;
+                    break;
+                case 1:
+                    count1++;
+                    break;
+                case 2:
+                    count2++;
+                    break;
+            }
+        }
+        // traverse in the array again and put zeros first then 1 and at last 2
+        int i = 0;
+        while(count0 > 0){
+            nums[i++] = 0;
+            count0--;
+        }
+        while(count1 > 0){
+            nums[i++] = 1;
+            count1--;
+        }
+        while(count2 > 0){
+            nums[i++] = 2;
+            count2--;
+        }
+    }
+};
+
+/************************* TC O(n)  ****************************/
