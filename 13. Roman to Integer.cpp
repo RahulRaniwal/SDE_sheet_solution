96751d3b8d33a90a5948fca42d07cb264a1c2f30
Gojo_69 <59061631+RahulRaniwal@users.noreply.github.com>
2022-08-07 19:57:37 +0530

    13. Roman to Integer

---
+++ 13. Roman to Integer.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int n = s.length();
+        
+        unordered_map<char,int> map;
+        map.insert({'I' , 1});
+        map.insert({'V' , 5});
+        map.insert({'X' , 10});
+        map.insert({'L' , 50});
+        map.insert({'C' , 100});
+        map.insert({'D' , 500});
+        map.insert({'M' , 1000});
+        
+        int ans = map[s[n-1]];
+        for(int i = n-2; i >= 0; i--){
+            if(map[s[i]] < map[s[i+1]]){
+                ans -= map[s[i]];
+            }else{
+                ans += map[s[i]];
+            }
+        }
+        return ans;
+    }
+};
