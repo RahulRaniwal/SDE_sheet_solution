c1e4221a34883d0c82e060726a8b23b3d15e6b14
Gojo_69 <59061631+RahulRaniwal@users.noreply.github.com>
2022-08-14 01:24:56 +0530

    144. Binary Tree Preorder Traversal

---
+++ 144. Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void preorder(TreeNode* root , vector<int>& pre){
+        if(root == NULL){
+            return;
+        }
+        pre.push_back(root->val);
+        preorder(root->left , pre);
+        preorder(root->right , pre);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> pre;
+        preorder(root , pre);
+        return pre;
+    }
+};
