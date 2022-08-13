ea39eeb4d97b31571d21beb0766b3d704664cba8
Gojo_69 <59061631+RahulRaniwal@users.noreply.github.com>
2022-08-14 01:24:22 +0530

    94. Binary Tree Inorder Traversal

---
+++ 94. Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void inorder(TreeNode* root , vector<int>& in){
+        if(root == NULL){
+            return;
+        }
+        inorder(root->left , in);
+        in.push_back(root->val);
+        inorder(root->right , in);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> in;
+        inorder(root , in);
+        return in;
+    }
+};
